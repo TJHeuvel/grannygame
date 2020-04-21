dd52eae4584a36ac7aa60773b988e22ae699fde6
Tijmen van den Heuvel <info@tjheuvel.net>
2020-04-21 17:46:56 +0200

    Commit of initial project

---
+++ Assets/GrannyController.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class GrannyController : MonoBehaviour
+{
+    [SerializeField] private Animator animator;
+    [SerializeField] private float speed;
+    void Update()
+    {
+        Vector2 movement = Vector2.zero;
+        if (Input.GetKey(KeyCode.LeftArrow))
+            movement = Vector2.left * speed;
+        else if (Input.GetKey(KeyCode.RightArrow))
+            movement = Vector2.right * speed;
+
+        movement *= Time.deltaTime;
+        animator.SetFloat("velocity", movement.magnitude);
+        animator.SetInteger("dir", (int)Vector2.Dot(movement.normalized, Vector2.right));
+        transform.position += new Vector3(movement.x, movement.y, 0);
+    }
+}
